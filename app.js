5a6ad447044769fd7c76ece3bbc58e8feb30d6aa
Richard <richard.johannesson@gmail.com>
2023-01-16 11:37:36 +0100

    La till object

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var arr = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 console.log(arr);
+var obj = {
+    string: 'String',
+    number: 20,
+    sant: false
+};
+console.log(obj);
--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 const arr = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 
 console.log(arr);
+
+const obj = {
+    string: 'String',
+    number: 20,
+    sant: false,
+};
+
+console.log(obj);
